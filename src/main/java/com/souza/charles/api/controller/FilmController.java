95849e386cd84c5c6745e0ef6ee6cd537677f45b
souzafcharles <souzach@outlook.com>
2025-09-08 14:34:54 -0300

    feat: add controller and config class

---
+++ src/main/java/com/souza/charles/api/controller/FilmController.java
@@ -0,0 +1,45 @@
+package com.souza.charles.api.controller;
+
+import com.souza.charles.api.model.Film;
+import com.souza.charles.api.service.FilmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
+public class FilmController {
+
+    @Autowired
+    private FilmService filmService;
+
+    @GetMapping("/complete")
+    public Object[] getAllFilmsComplete() {
+        return filmService.findAllFilmsComplete();
+    }
+
+    @GetMapping
+    public Film[] getAllFilms() {
+        return filmService.findAllFilms();
+    }
+
+    @GetMapping("/title")
+    public List<Film> getFilmsByTitle(@RequestParam("q") String title) {
+        return filmService.findFilmsByTitle(title);
+    }
+
+    @GetMapping("/directors")
+    public Map<Integer, String> getDirectors() {
+        return filmService.findDirectors();
+    }
+
+    @GetMapping("/newest")
+    public List<Film> getNewestFilms() {
+        return filmService.findFilmsByNewest();
+    }
+}
