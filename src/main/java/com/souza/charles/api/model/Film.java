06684c2e62304a1ee203cf4a54f783db355104d6
souzafcharles <souzach@outlook.com>
2025-09-08 12:22:24 -0300

    feat: add entity class

---
+++ src/main/java/com/souza/charles/api/model/Film.java
@@ -0,0 +1,48 @@
+package com.souza.charles.api.model;
+
+public class Film {
+
+    private String title;
+    private String original_title;
+    private String original_title_romanised;
+    private String description;
+    private String director;
+    private String producer;
+    private String release_date;
+    private String image;
+
+    public Film() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOriginal_title() {
+        return original_title;
+    }
+
+    public String getOriginal_title_romanised() {
+        return original_title_romanised;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public String getImage() {
+        return image;
+    }
+}
