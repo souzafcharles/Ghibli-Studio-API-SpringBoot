eafb2a91865f3db10e14bac9990bfbb2a668053a
souzafcharles <souzach@outlook.com>
2025-09-08 12:16:24 -0300

    chore: initial project setup

---
+++ src/main/java/com/souza/charles/api/StudioGhibliApiApplication.java
@@ -0,0 +1,13 @@
+package com.souza.charles.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioGhibliApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudioGhibliApiApplication.class, args);
+	}
+
+}
